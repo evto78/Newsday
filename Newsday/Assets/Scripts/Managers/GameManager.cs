3f3ec1d6c740b6fec1a2ddefd0fdde3b784a2f82
Whipserh <mcbrider@sheridancollege.ca>
2026-02-17 14:44:50 -0500

    Added win cards
    Lets the player if they got a article right or wrong

--- Newsday/Assets/Scripts/Managers/GameManager.cs
+++ Newsday/Assets/Scripts/Managers/GameManager.cs
@@ -1,16 +1,18 @@
 using System.Collections;
+using UnityEditor.VersionControl;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private Checklist checklist;
     [SerializeField] private ArticleManager articleManager;
-
+    [SerializeField] private GameObject tempWin, tempLose;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        tempWin.SetActive(false);
+        tempLose.SetActive(false);
     }
 
     // Update is called once per frame
@@ -19,16 +21,24 @@ public class GameManager : MonoBehaviour
         
     }
 
+    IEnumerator displayMessage(GameObject result)
+    {
+        result.SetActive(true);
+        yield return new WaitForSeconds(3f);
+        result.SetActive(false);
+    }
     
     public void validateResponse()
     {
         if (!compareArticle())
         {
-            Debug.Log("You did something wrong");
+            //Debug.Log("You did something wrong");
+            StartCoroutine(displayMessage(tempLose));
         }
         else
         {
-            Debug.Log("You  got it right!!!!");
+            //Debug.Log("You  got it right!!!!");
+            StartCoroutine(displayMessage(tempWin));
         }
     }
 
